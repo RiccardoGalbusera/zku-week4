be1a256cdb5d234415dbbc7ba66ef7f98778ee4e
RiccardoGalbusera <93911514+RiccardoGalbusera@users.noreply.github.com>
2022-05-30 22:08:19 +0200

    zku - week4 assignment

--- frontend_assignment/pages/index.tsx
+++ frontend_assignment/pages/index.tsx
@@ -1,13 +1,26 @@
+import { useEffect } from "react";
 import detectEthereumProvider from "@metamask/detect-provider"
 import { Strategy, ZkIdentity } from "@zk-kit/identity"
 import { generateMerkleProof, Semaphore } from "@zk-kit/protocols"
-import { providers } from "ethers"
+import { ethers, providers } from "ethers"
+import GreetersJson from "artifacts/contracts/Greeters.sol/Greeters.json";
 import Head from "next/head"
 import React from "react"
 import styles from "../styles/Home.module.css"
 
 export default function Home() {
     const [logs, setLogs] = React.useState("Connect your wallet and greet!")
+
+    useEffect(() => {
+        const provider = new providers.JsonRpcProvider("http://localhost:8545")
+        const signer = provider.getSigner()
+        const contract = new ethers.Contract("0xe7f1725E7734CE288F8367e1Bb143E90bb3F0512", GreetersJson.abi, signer)
+
+        contract.on("NewGreeting", (m: any) => {
+            const message = ethers.utils.parseBytes32String(m)
+            console.warn("Greet:" + message);
+        })
+    }, [])
 
     async function greet() {
         setLogs("Creating your Semaphore identity...")
@@ -62,7 +75,7 @@ export default function Home() {
     return (
         <div className={styles.container}>
             <Head>
-                <title>Greetings</title>
+                <title>Assignment form</title>
                 <meta name="description" content="A simple Next.js/Hardhat privacy application with Semaphore." />
                 <link rel="icon" href="/favicon.ico" />
             </Head>
